3d2d9c8f2600adbdbd0e451a8f6858104293e063
Mikrzak <mikolajjuszczak17@gmail.com>
2024-12-19 00:32:18 +0100

    try-with-resources dla wpisywania z pliku (zamykanie buforu)

--- src/Simulation/AutoGeneration.java
+++ src/Simulation/AutoGeneration.java
@@ -25,14 +25,13 @@ public class AutoGeneration implements GenerationMethod{
     private boolean readNamesFromFile(){
 
         String line;
-        System.out.println(this.getClass());
 
         Field[] fields = this.getClass().getDeclaredFields();
 
         for(int i = 0; i < filePaths.length; i++) {
 
-            try {
-                BufferedReader reader = new BufferedReader(new FileReader(filePaths[i]));
+            try (BufferedReader reader = new BufferedReader(new FileReader(filePaths[i]))){
+
                 fields[i].setAccessible(true);
 
                 ArrayList<String> list = new ArrayList<>();
@@ -71,10 +70,8 @@ public class AutoGeneration implements GenerationMethod{
     private boolean readIllnessesFromFile(){
 
         String line;
-        //System.out.println(this.getClass());
 
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(illnessPath));
+        try (BufferedReader reader = new BufferedReader(new FileReader(illnessPath))){
 
             ArrayList<String> lines = new ArrayList<>();
 
@@ -90,7 +87,7 @@ public class AutoGeneration implements GenerationMethod{
             e.printStackTrace();
             return false;
         }
-
+        
         return true;
     }
 
